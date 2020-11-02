aa11e4bac75f6640f0dd83bd2baae313a26c7124
O'Donald Hudson <odonaldhudson@gmail.com>
2020-11-02 17:24:40 -0600

    coursera week 1

---
+++ courserawk1_week1_basic_data_structures/1_brackets_in_code/check_brackets.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Stack;
+
+class Bracket {
+    Bracket(char type, int position) {
+        this.type = type;
+        this.position = position;
+    }
+
+    boolean Match(char c) {
+        if (this.type == '[' && c == ']')
+            return true;
+        if (this.type == '{' && c == '}')
+            return true;
+        if (this.type == '(' && c == ')')
+            return true;
+        return false;
+    }
+
+    char type;
+    int position;
+}
+
+class check_brackets {
+    public static void main(String[] args) throws IOException {
+        InputStreamReader input_stream = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input_stream);
+        String text = reader.readLine();
+
+        Stack<Bracket> opening_brackets_stack = new Stack<Bracket>();
+        for (int position = 0; position < text.length(); ++position) {
+            char next = text.charAt(position);
+
+            if (next == '(' || next == '[' || next == '{') {
+                // Process opening bracket, write your code here
+            }
+
+            if (next == ')' || next == ']' || next == '}') {
+                // Process closing bracket, write your code here
+            }
+        }
+
+        // Printing answer, write your code here
+    }
+}
+
+		if(ans == -1 && opening_brackets_stack.isEmpty())
+			System.out.println("Success");
+		else if(ans == -1)
+			System.out.println(opening_brackets_stack.peek().position+1);
+		else
+			System.out.println(ans);
+	}
+}
